8f520310d71c861db54f578061c8dad9c53018dd
WickyChandra <WickyChandra@DESKTOP-846NUOS>
2022-06-01 10:33:35 +0700

    fix typo

--- MiniProjectSpringAPI/src/main/java/com/miniproject/databaseprofile/repository/impl/ProfilepageDAOimpl.java
+++ MiniProjectSpringAPI/src/main/java/com/miniproject/databaseprofile/repository/impl/ProfilepageDAOimpl.java
@@ -19,8 +19,8 @@ public class ProfilepageDAOimpl implements ProfilepageDAO {
 	public List<Profilepage> selectAll() {
 		return jdbc.query("select p.name, e.univ_nm as univ_name , e.grad_yr, p.phone, g.nameg as gendr, "
 				+ "sc.skill1 as skill1, sc.skill2 as skill2, "
-				+ "sc.skill3 as skill3, sc.skill4 as skill4 from profile p \r\n"
-				+ "join edushow e on e.id = p.id\r\n"
+				+ "sc.skill3 as skill3, sc.skill4 as skill4 from profile p "
+				+ "join edushow e on e.id = p.id "
 				+ "join gender g on g.id = p.gend_id "
 				+ "join skillshow sc on sc.id = p.id;", new BeanPropertyRowMapper<Profilepage>(Profilepage.class));
 	}
@@ -28,9 +28,12 @@ public class ProfilepageDAOimpl implements ProfilepageDAO {
 	@Override
 	public List<Profilepage> showpage(int pg) {
 		// TODO Auto-generated method stub
-		return jdbc.query("select p.name, e.univ_nm, e.grad_yr, p.phone, g.nameg from profile p \r\n"
-				+ "join temp e on e.id = p.id and p.id > ?\r\n"
-				+ "join gender g on g.id = p.gend_id \r\n"
+		return jdbc.query("select p.name, e.univ_nm as univ_name , e.grad_yr, p.phone, g.nameg as gendr, "
+				+ "sc.skill1 as skill1, sc.skill2 as skill2, "
+				+ "sc.skill3 as skill3, sc.skill4 as skill4 from profile p "
+				+ "join edushow e on e.id = p.id and (p.id > ?) "
+				+ "join gender g on g.id = p.gend_id "
+				+ "join skillshow sc on sc.id = p.id "
 				+ "limit 10;", new BeanPropertyRowMapper<Profilepage>(Profilepage.class),((pg-1)*10));
 	}
 }
